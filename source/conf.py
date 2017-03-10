52982a33db3e0f3e03c52eaff2045e2faf79b5ce
David Phan <david.phan@wephone.io>
2017-03-10 11:26:35 +0100

    some change in config

--- source/conf.py
+++ source/conf.py
@@ -20,6 +20,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -64,7 +65,7 @@ release = u'1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -83,7 +84,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -102,6 +103,7 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'wePhonedoc'
 
+html_show_sourcelink = False
 
 # -- Options for LaTeX output ---------------------------------------------
 
--- source/index.rst
+++ source/index.rst
@@ -3,12 +3,17 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to wePhone's documentation!
+wePhone's documentation!
 ===================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+
+   user_management
+   number_management
+   call_queue_management
+
 
 
 
