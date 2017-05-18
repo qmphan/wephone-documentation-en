0b2297a726efd651d3b939e2a1f25517ae525110
David Phan <david.phan@wephone.io>
2017-05-18 16:03:01 +0200

    added sphinx.ext.autosectionlabel to conf.py

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,9 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.todo',
-    'sphinx.ext.ifconfig']
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.autosectionlabel'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
